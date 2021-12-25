de5f89ab9220309193860be8c00b337fd1528858
deanchen <antlet2004@gmail.com>
2021-12-25 14:53:00 +0800

    chore: remove useless lines

--- .jest/globalSetup.ts
+++ .jest/globalSetup.ts
@@ -1,5 +1,2 @@
 export default function globalSetup() {
-  process.env.DATABASE_URL ??= 'postgresql://postgres:passwrd@0.0.0.0/postgres'
-  process.env.DATABASE_SCHEMA ??= 'map_features'
-  process.env.RABBITMQ_URL ??= 'amqp://guest:guest@localhost'
 }
