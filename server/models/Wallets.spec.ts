4463aa23bf59c3ec552bac953de2e16f9d903f05
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-01-26 11:54:28 -0400

    test: fix the test

--- server/models/Wallets.spec.ts
+++ server/models/Wallets.spec.ts
@@ -1,21 +1,22 @@
-import CountryModel from './Country';
+import WalletsModel from './Wallets';
 
-describe('/wallets', () => {
-  it('getWallets', async () => {
-    const wallet = {
-      id: '0248f77a-1531-11ec-82a8-0242ac130003',
-      name: '180Earth',
-      token_in_wallet: 22,
-      photo_url: "'https://180.earth/wp-content/uploads/2020/01/Asset-1.png",
+describe('/wallet', () => {
+  it('get wallet by id or name', async () => {
+    const wallets = {
+      id: '88a33d5b-5c47-4a32-8572-0899817d3f38',
+      name: 'NewWalletByAutoTool_49836',
+      password: null,
+      salt: null,
+      logo_url: null,
+      created_at: '2021-10-08T02:33:20.732Z',
     };
-
     const repo: any = {
-      getById: jest.fn(() => Promise.resolve(wallet)),
+      getByIdOrName: jest.fn(() => Promise.resolve(wallets)),
     };
     // @ts-ignore
-    const execute = CountryModel.getById(repo);
+    const execute = WalletsModel.getByIdOrName(repo);
     const result = await execute(1);
-    expect(result).toMatchObject(result);
-    expect(repo.getById).toBeCalledWith(1);
+    expect(result).toMatchObject(wallets);
+    expect(repo.getByIdOrName).toBeCalledWith(1);
   });
 });
