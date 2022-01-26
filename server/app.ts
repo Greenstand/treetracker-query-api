5558fe25d897f0d955a8d4cc73566fd11caca11a
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-01-26 12:05:57 -0400

    feat: add walletrouter

--- server/app.ts
+++ server/app.ts
@@ -9,28 +9,36 @@ import treesRouter from './routers/treesRouter';
 import plantersRouter from './routers/plantersRouter';
 import organizationsRouter from './routers/organizationsRouter';
 import speciesRouter from './routers/speciesRouter';
+import walletsRouter from './routers/walletsRouter';
 import cors from 'cors';
-
-
 const app = express();
 const config = require('../config/config.js');
 
 //Sentry.init({ dsn: config.sentry_dsn });
 
-// app allow cors 
+// app allow cors
 app.use(cors());
 
 /*
  * Check request
  */
-app.use(handlerWrapper(async (req, _res, next) => {
-  if(req.method === "POST" || req.method === "PATCH"  || req.method === "PUT" ){
-    if(req.headers['content-type'] !== "application/json"){
-    throw new HttpError(415, "Invalid content type. API only supports application/json");
+app.use(
+  handlerWrapper(async (req, _res, next) => {
+    if (
+      req.method === 'POST' ||
+      req.method === 'PATCH' ||
+      req.method === 'PUT'
+    ) {
+      if (req.headers['content-type'] !== 'application/json') {
+        throw new HttpError(
+          415,
+          'Invalid content type. API only supports application/json',
+        );
+      }
     }
-  }
-  next();
-}));
+    next();
+  }),
+);
 
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
@@ -41,12 +49,13 @@ app.use('/trees', treesRouter);
 app.use('/planters', plantersRouter);
 app.use('/organizations', organizationsRouter);
 app.use('/species', speciesRouter);
+app.use('/wallets', walletsRouter);
 // Global error handler
 app.use(errorHandler);
 
-const version = require('../package.json').version
-app.get('*',function (req, res) {
-  res.status(404).send(version)
+const version = require('../package.json').version;
+app.get('*', function (req, res) {
+  res.status(404).send(version);
 });
 
 export default app;
