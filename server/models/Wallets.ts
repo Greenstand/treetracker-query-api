09ab42c2b51847231917f92f51bb61604b51dd43
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-01-28 06:14:07 -0400

    feat: add wallets

--- server/models/Wallets.ts
+++ server/models/Wallets.ts
@@ -1,17 +1,10 @@
+import Wallets from 'interfaces/Wallets';
 import { delegateRepository } from '../infra/database/delegateRepository';
 // eslint-disable-next-line import/no-cycle
 import WalletsRepository from '../infra/database/WalletsRepository';
 
-export type Wallet = {
-  id: string;
-  name: string;
-  token_in_wallet: number;
-  photo_url: string;
-};
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 export default {
-  getWalletByIdOrName: delegateRepository<WalletsRepository>(
+  getWalletByIdOrName: delegateRepository<WalletsRepository, Wallets>(
     'getWalletByIdOrName',
   ),
 };
