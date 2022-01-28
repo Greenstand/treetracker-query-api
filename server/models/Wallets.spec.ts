bc79d3cc435400aa702133e1dc0b46356ccbfbd6
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-01-28 06:12:44 -0400

    fix: change getbyidorname to getwalletbyidorname

--- server/models/Wallets.spec.ts
+++ server/models/Wallets.spec.ts
@@ -1,6 +1,6 @@
 import WalletsModel from './Wallets';
 
-describe('/wallet', () => {
+describe('/wallets', () => {
   it('get wallet by id or name', async () => {
     const wallets = {
       id: '88a33d5b-5c47-4a32-8572-0899817d3f38',
@@ -12,11 +12,11 @@ describe('/wallet', () => {
     };
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const repo: any = {
-      getByIdOrName: jest.fn(() => Promise.resolve(wallets)),
+      getWalletByIdOrName: jest.fn(() => Promise.resolve(wallets)),
     };
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const execute = WalletsModel.getByIdOrName(repo);
+    const execute = WalletsModel.getWalletByIdOrName(repo);
     const result = await execute(1);
     expect(result).toMatchObject(wallets);
     expect(repo.getWalletByIdOrName).toBeCalledWith(1);
