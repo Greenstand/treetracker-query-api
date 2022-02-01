932ec465fa6c065d4612bcb8f24cc04221d68905
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-02-01 08:41:49 -0400

    feat: add tokens interface

---
+++ server/interfaces/Tokens.ts
@@ -0,0 +1,11 @@
+export default interface Tokens {
+  id: number | string;
+  wallet_id: string;
+  capture_id: number;
+  capture_photo_url: string;
+  planter_id: number;
+  planter_first_name: string;
+  planter_last_name: string;
+  planter_photo_url: string;
+  created_at: string;
+}
