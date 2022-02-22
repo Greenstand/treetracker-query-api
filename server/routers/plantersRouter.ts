a901ba791257a8b33fda70ab69ab59981bceb39e
dagmawig <dgebreselasse@gmail.com>
2022-02-21 22:49:50 -0600

    feat: add planter name api endpoint

--- server/routers/plantersRouter.ts
+++ server/routers/plantersRouter.ts
@@ -27,31 +27,50 @@ router.get(
     Joi.assert(
       req.query,
       Joi.object().keys({
+        keyword: Joi.string(),
         organization_id: Joi.number().integer().min(0),
         limit: Joi.number().integer().min(1).max(1000),
         offset: Joi.number().integer().min(0),
       }),
     );
-    const { limit = 20, offset = 0, organization_id } = req.query;
+    const { limit = 20, offset = 0, organization_id, keyword } = req.query;
     const repo = new PlanterRepository(new Session());
     const filter: Filter = {};
     if (organization_id) {
       filter.organization_id = organization_id;
     }
-    const result = await PlanterModel.getByFilter(repo)(filter, {
-      limit,
-      offset,
-    });
-    res.send({
-      total: null,
-      offset,
-      limit,
-      planters: result.map((planter) => ({
-        ...planter,
-        links: PlanterModel.getPlanterLinks(planter),
-      })),
-    });
-    res.end();
+
+    if (keyword !== undefined) {
+      const result = await PlanterModel.getByName(repo)(keyword, {
+        limit,
+        offset,
+      });
+      res.send({
+        total: null,
+        offset,
+        limit,
+        planters: result.map((planter) => ({
+          ...planter,
+          links: PlanterModel.getPlanterLinks(planter),
+        })),
+      });
+      res.end();
+    } else {
+      const result = await PlanterModel.getByFilter(repo)(filter, {
+        limit,
+        offset,
+      });
+      res.send({
+        total: null,
+        offset,
+        limit,
+        planters: result.map((planter) => ({
+          ...planter,
+          links: PlanterModel.getPlanterLinks(planter),
+        })),
+      });
+      res.end();
+    }
   }),
 );
 
