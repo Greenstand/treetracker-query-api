9d39e72ce8c9bb0bf0064f32b85137d216c3e085
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-25 14:23:50 -0800

    test(wip): trees.spec uses mock data
    test(mock): update mock tree data

--- __tests__/e2e/trees.spec.ts
+++ __tests__/e2e/trees.spec.ts
@@ -1,67 +1,61 @@
+import exampleTree from '@mocks/trees/912681.json';
 import supertest from 'supertest';
+import knex, { TableNames } from 'infra/database/knex';
 import app from '../../server/app';
 
-describe('trees', () => {
-  it('trees/{id}', async () => {
-    const response = await supertest(app).get('/trees/912681');
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchObject({
-      id: 912681,
-      lat: expect.anything(),
-      lon: expect.anything(),
-    });
-  });
-
-  it('Unknown tree', async () => {
-    const response = await supertest(app).get('/trees/1');
-    expect(response.status).toBe(404);
-  });
-
-  it('trees?limit=1&offset=0', async () => {
-    const response = await supertest(app).get('/trees?limit=1&offset=0');
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchObject({
-      trees: expect.anything(),
-    });
-  });
-
-  it('trees?limit=1&offset=0', async () => {
-    const response = await supertest(app).get('/trees?limit=1&offset=0');
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchObject({
-      trees: expect.anything(),
-    });
-  });
-
-  it(
-    'trees?limit=1&organization_id=11',
-    async () => {
-      const response = await supertest(app).get(
-        '/trees?limit=1&organization_id=11',
-      );
-      expect(response.status).toBe(200);
-      expect(response.body).toMatchObject({
-        trees: expect.anything(),
-      });
-      expect(response.body.trees[0]).toMatchObject({
-        organization_id: 11,
-      });
-    },
-    1000 * 60,
-  );
-
-  it(
-    'trees/featured',
-    async () => {
-      const response = await supertest(app).get('/trees/featured');
-      expect(response.status).toBe(200);
-      // expect(response.body).toMatchObject({
-      //   trees: expect.anything(),
-      // });
-      expect(response.body.trees[0]).toMatchObject({
-        id: expect.any(Number),
-      });
-    },
-    1000 * 10,
-  );
+beforeAll(async () => {
+  await knex(TableNames.Trees).insert(exampleTree);
 });
+
+it('trees/{id}', async () => {
+  const response = await supertest(app).get(`/trees/${exampleTree.id}`);
+  expect(response.status).toBe(200);
+  expect(response.body).toMatchObject({
+    id: exampleTree.id,
+    lat: expect.anything(),
+    lon: expect.anything(),
+  });
+});
+
+it('Unknown tree', async () => {
+  const response = await supertest(app).get('/trees/1');
+  expect(response.status).toBe(404);
+});
+
+it('trees?limit=1&offset=0', async () => {
+  const response = await supertest(app).get('/trees?limit=1&offset=0');
+  expect(response.status).toBe(200);
+  expect(response.body).toMatchObject({
+    trees: expect.anything(),
+  });
+});
+
+it(
+  'Get tree by organization id',
+  async () => {
+    const organization_id = exampleTree.planting_organization_id;
+    const response = await supertest(app).get(
+      `/trees?limit=1&organization_id=${organization_id}`,
+    );
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      trees: expect.anything(),
+    });
+    expect(response.body.trees[0]).toMatchObject({
+      organization_id,
+    });
+  },
+  1000 * 60,
+);
+
+it(
+  'trees/featured',
+  async () => {
+    const response = await supertest(app).get('/trees/featured');
+    expect(response.status).toBe(200);
+    expect(response.body.trees[0]).toMatchObject({
+      id: expect.any(Number),
+    });
+  },
+  1000 * 10,
+);
