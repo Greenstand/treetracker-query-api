8d95a5db30c1c881b3efdec2a15aea45b0240784
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-25 20:40:51 -0800

    test(setup): conditionally seed db before testing

--- .jest/globalSetup.ts
+++ .jest/globalSetup.ts
@@ -1,3 +1,12 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-export default function globalSetup() {}
+import exampleTree from '../docs/api/spec/examples/trees/912681.json';
+// import exampleWallet from '@mocks/wallets/exampleWallet.json';
+import knex, { TableNames } from '../server/infra/database/knex';
+
+export default async function globalSetup() {
+  if (process.env.SEED === 'true') {
+    await knex(TableNames.Trees).insert(exampleTree);
+  }
+  knex.destroy();
+}
