e0d3a24044774523334a8bef85467d5b8e07f0ac
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-03-01 00:45:51 -0800

    test(species): use seed data

--- __tests__/e2e/species.spec.ts
+++ __tests__/e2e/species.spec.ts
@@ -1,47 +1,48 @@
+import exampleOrganizations from '@mocks/organizations/exampleOrganization.json';
+import examplePlanter from '@mocks/planters/examplePlanter.json';
+import exampleSpecies from '@mocks/species/exampleSpecies.json';
 import supertest from 'supertest';
-import app from '../../server/app';
+import app from 'app';
 
-describe('species', () => {
-  it('species/{id}', async () => {
-    const response = await supertest(app).get('/species/8');
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchObject({
-      id: 8,
-      name: expect.any(String),
-    });
+it('species/{id}', async () => {
+  const response = await supertest(app).get(`/species/${exampleSpecies.id}`);
+  expect(response.status).toBe(200);
+  expect(response.body).toMatchObject({
+    id: 8,
+    name: expect.any(String),
   });
-
-  it(
-    'species?organization_id=1&limit=1',
-    async () => {
-      const response = await supertest(app).get(
-        '/species?organization_id=1&limit=1',
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.species).toBeInstanceOf(Array);
-      expect(response.body.species[0]).toMatchObject({
-        total: expect.stringMatching(/\d+/),
-        name: expect.any(String),
-        id: expect.any(Number),
-      });
-    },
-    1000 * 30,
-  );
-
-  it(
-    'species?planter_id=1&limit=1',
-    async () => {
-      const response = await supertest(app).get(
-        '/species?planter_id=1&limit=1',
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.species).toBeInstanceOf(Array);
-      expect(response.body.species[0]).toMatchObject({
-        total: expect.stringMatching(/\d+/),
-        name: expect.any(String),
-        id: expect.any(Number),
-      });
-    },
-    1000 * 30,
-  );
 });
+
+it(
+  'species?organization_id=1&limit=1',
+  async () => {
+    const response = await supertest(app).get(
+      `/species?organization_id=${exampleOrganizations.id}&limit=1`,
+    );
+    expect(response.status).toBe(200);
+    expect(response.body.species).toBeInstanceOf(Array);
+    expect(response.body.species[0]).toMatchObject({
+      total: expect.stringMatching(/\d+/),
+      name: expect.any(String),
+      id: expect.any(Number),
+    });
+  },
+  1000 * 30,
+);
+
+it(
+  'species?planter_id=1&limit=1',
+  async () => {
+    const response = await supertest(app).get(
+      `/species?planter_id=${examplePlanter.id}&limit=1`,
+    );
+    expect(response.status).toBe(200);
+    expect(response.body.species).toBeInstanceOf(Array);
+    expect(response.body.species[0]).toMatchObject({
+      total: expect.stringMatching(/\d+/),
+      name: expect.any(String),
+      id: expect.any(Number),
+    });
+  },
+  1000 * 30,
+);
