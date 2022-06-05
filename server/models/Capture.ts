0145bbc780da78130f4dc1147c929984fbc59b11
Gwynn Dandridge-Perry <gwynndp@gmail.com>
2022-06-05 14:52:38 -0700

    feat: update captures queries

--- server/models/Capture.ts
+++ server/models/Capture.ts
@@ -1,7 +1,20 @@
+import log from 'loglevel';
 import CaptureRepository from 'infra/database/CaptureRepository';
 import { delegateRepository } from 'infra/database/delegateRepository';
 import Capture from 'interfaces/Capture';
+import CaptureFilter from 'interfaces/CaptureFilter';
+import FilterOptions from 'interfaces/FilterOptions';
+
+function getByFilter(
+  captureRepository: CaptureRepository,
+): (filter: CaptureFilter, options: FilterOptions) => Promise<Capture[]> {
+  return async function (filter: CaptureFilter, options: FilterOptions) {
+    const captures = await captureRepository.getByFilter(filter, options);
+    return captures;
+  };
+}
 
 export default {
+  getByFilter,
   getById: delegateRepository<CaptureRepository, Capture>('getById'),
 };
